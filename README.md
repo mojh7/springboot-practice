d69af627e96d707a571b20c5976a42e20e033e4c
momojh94 <mjh79017901@gmail.com>
2021-07-26 19:49:07 +0900

    기본 설정

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# file-example
- file upload / download example
+# springboot-practice
+
+ 여러가지 테스트해보기 위한 스프링부트 프로젝트
---
+++ src/main/java/com/example/api/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.example.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping("/hello")
+    public ResponseEntity helloWorld() {
+        return ResponseEntity.ok("hello world~");
+    }
+}
